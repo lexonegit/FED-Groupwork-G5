0c3820014ddfb60ce3fa6f74c99d9b592f59827b
kripi-png <66375160+kripi-png@users.noreply.github.com>
2022-04-10 13:22:33 +0300

    total expense calculations

--- car-fuel-calculator/src/components/TotalExpenses.js
+++ car-fuel-calculator/src/components/TotalExpenses.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TotalExpenses = () => {
+  const { expenses } = useContext(GlobalContext);
+
+  const totalSum = expenses.reduce((acc, expense) => acc + expense.cost, 0);
+  const totalConsumption = expenses.reduce((acc, expense) => acc + expense.amount, 0);
+  const totalDistance = expenses.reduce((acc, expense) => acc + expense.distance, 0);
+
+  const avgExpensesPer100 = totalSum / totalDistance * 100;
+  const avgConsumptionPer100 = totalConsumption / totalDistance * 100;
+
   return (
     <div>
       <h4>Total refueling expenses (all cars)</h4>
-      <p>Total sum (euros): </p>
-      <p>Total consumption (liters): </p>
-      <p>Total distance (km): </p>
-      <p>Avg. expenses per 100 km (euros): </p>
-      <p>Avg. consumption per 100 km (liters): </p>
+      <p>Total sum (euros): {totalSum}</p>
+      <p>Total consumption (liters): {totalConsumption}</p>
+      <p>Total distance (km): {totalDistance} </p>
+      <p>Avg. expenses per 100 km (euros): {avgExpensesPer100}</p>
+      <p>Avg. consumption per 100 km (liters): {avgConsumptionPer100}</p>
     </div>
   )
 }
--- car-fuel-calculator/src/context/GlobalState.js
+++ car-fuel-calculator/src/context/GlobalState.js
@@ -4,10 +4,10 @@ import AppReducer from './AppReducer';
 // Initial state
 const initialState = {
   expenses: [
-    { id: 1, carName: 'Car 1', amount: 60, cost: 30, },
-    { id: 2, carName: 'Car 2', amount: 100, cost: 50, },
-    { id: 3, carName: 'Car 1', amount: 120, cost: 60, },
-    { id: 4, carName: 'Car 2', amount: 30, cost: 15, },
+    { id: 1, carName: 'Car 1', amount: 60, cost: 30, distance: 0, },
+    { id: 2, carName: 'Car 2', amount: 100, cost: 50, distance: 0, },
+    { id: 3, carName: 'Car 1', amount: 120, cost: 60, distance: 0, },
+    { id: 4, carName: 'Car 2', amount: 30, cost: 15, distance: 0, },
   ],
   cars: [
     { id: 1, carName: 'Auto 1' },
